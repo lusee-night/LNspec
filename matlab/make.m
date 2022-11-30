6a1aa3e083e182fb6877334ddc3213a648e20c96
Raguzin <eraguzin@silicon.inst.bnl.gov>
2022-11-30 12:58:14 -0500

    some git weirdness, sorry

--- matlab/make.m
+++ matlab/make.m
@@ -10,12 +10,8 @@ fixptcfg.LaunchNumericTypesReport = true;
 % Create an 'hdl' config with default settings
 hdlcfg = coder.config('hdl');
 hdlcfg.TestBenchName = 'variance_tb';
-<<<<<<< HEAD
 hdlcfg.GenerateHDLTestBench = true;
-%codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),0,0,0,0} convolver
-=======
 codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),0,0,0,0} convolver
->>>>>>> 1aeb594a72550e9689cac4dae449f013c2559eea
 %codegen -float2fixed fixptcfg -config hdlcfg -args {} weight_streamer
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0)} sfft 
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),true} pk_accum
