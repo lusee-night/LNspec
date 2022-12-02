82e9c55184da05d241f398b7700e34adc0588c5e
Anze Slosar <slosar@gmail.com>
2022-12-02 06:07:52 -0500

    classified convolver

--- matlab/make.m
+++ matlab/make.m
@@ -10,7 +10,8 @@ fixptcfg.LaunchNumericTypesReport = true;
 % Create an 'hdl' config with default settings
 hdlcfg = coder.config('hdl');
 hdlcfg.TestBenchName = 'variance_tb';
-codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),0,0,0,0} convolver
+codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),0,0,0,0} weight_fold
+%process
 %codegen -float2fixed fixptcfg -config hdlcfg -args {} weight_streamer
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0)} sfft 
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),true} pk_accum
--- matlab/spectrometer_tb.m
+++ matlab/spectrometer_tb.m
@@ -10,15 +10,22 @@ Npk = 0;
 pk=zeros(4,settings_Nchan);
 clf;
 
+weight_fold1 = weight_fold; 
+convolver1 = @weight_fold1.process;
+weight_fold2 = weight_fold; 
+convolver2 = @weight_fold2.process;
+
 while Npk<2;
     sample1 = int16(28000*sin(omega1*t/settings_Nfft*2*pi)+2000*sin(omegax*t/settings_Nfft*2*pi));
     sample2 = int16(28000*sin(omega2*t/settings_Nfft*2*pi)+2000*cos(omegax*t/settings_Nfft*2*pi));
 
     [w1,w2,w3,w4] = weight_streamer();
-    acc1 = convolver(sample1, w1, w2, w3, w4);
-    acc2 = convolver(sample2, w1, w2, w3, w4);
+    acc1 = convolver1(sample1, w1, w2, w3, w4);
+    acc2 = convolver2(sample2, w1, w2, w3, w4);
+    
     val = complex(acc1,acc2);
-    [fft_out, fft_valid] = sfft(val')
+    [fft_out, fft_valid] = sfft(val');
+
     [pks, ready] = pk_accum(fft_out,fft_valid);
     if ready
         pk(:,count) = pks;
@@ -37,11 +44,13 @@ freq = 1:settings_Nchan;
 pk1 = (pk(1,:) + pk(2,:) + 2 * pk(3,:))/4.0;
 pk2 = (pk(1,:) + pk(2,:) - 2 * pk(3,:))/4.0;
 pkXR = pk(4,:)/2;
-pkXI = (pk(1,:)-pk(2,:))/4.0
+pkXI = (pk(1,:)-pk(2,:))/4.0;
 clf;
 
 plot(freq,pk1,'bo-');
 hold on
 plot(freq,pk2,'ro-');
+xlim([0 100]);
+
 %semilogy(freq,pk2,'ro-');
 
