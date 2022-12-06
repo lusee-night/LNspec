b112c7cc497d03149a5c7f3967bb2227fea32a42
Raguzin <eraguzin@silicon.inst.bnl.gov>
2022-12-06 09:48:48 -0500

    Changes during live session on 12/6

--- matlab/average.m
+++ matlab/average.m
@@ -1,4 +1,4 @@
-function [outpk,ready] = average(P1,P2,PR,PI, count, ready)
+function [outpk,ready_out] = average(P1,P2,PR,PI, count, ready_in)
     persistent  Nac accum outbuf stream;
     %assert(isa(count,'int16'))
     if isempty(Nac)
@@ -7,7 +7,7 @@ function [outpk,ready] = average(P1,P2,PR,PI, count, ready)
         outbuf = zeros(4,coder.const(settings_Nchan));
         stream = 0;
     end
-    if (ready)
+    if (ready_in)
         accum(1,count) = accum(1,count) + P1;
         accum(2,count) = accum(2,count) + P2;
         accum(3,count) = accum(3,count) + PR;
@@ -25,14 +25,14 @@ function [outpk,ready] = average(P1,P2,PR,PI, count, ready)
     end
         
     if stream
-        ready = true;
+        ready_out = true;
         outpk = outbuf(:,stream);
         stream = stream + 1;
         if stream>coder.const(settings_Nchan)
             stream  = 0;
         end
     else
-        ready = false;
+        ready_out = false;
         outpk = zeros(4,1);
     end
 end
--- matlab/make.m
+++ matlab/make.m
@@ -9,7 +9,7 @@ fixptcfg.LaunchNumericTypesReport = true;
 
 % Create an 'hdl' config with default settings
 hdlcfg = coder.config('hdl');
-hdlcfg.TestBenchName = 'variance_tb';
+hdlcfg.TestBenchName = 'spectrometer_tb';
 
 hdlcfg.GenerateHDLTestBench = true;
 
@@ -17,8 +17,8 @@ hdlcfg.GenerateHDLTestBench = true;
 %codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),0,0,0,0} weight_fold
 %codegen -float2fixed fixptcfg -config hdlcfg -args {} weight_streamer
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0)} sfft 
-%codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),true} correlate
-codegen -float2fixed fixptcfg -config hdlcfg -args {0.1, 0.1, 0.1, 0.1, int16(1), true} average
+codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),true} correlate
+%codegen -float2fixed fixptcfg -config hdlcfg -args {0.1, 0.1, 0.1, 0.1, int16(1), true} average
 %codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),int16(0)} spectrometer
 
 
