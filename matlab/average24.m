644446e83f15d5ade431f74dad4772c6639155f9
Anze Slosar <slosar@gmail.com>
2022-12-07 16:30:05 -0500

    even worse

---
+++ matlab/average24.m
@@ -0,0 +1,37 @@
+function [outpk,ready_out] = average24(P1, count, ready_in)
+    persistent  Nac P1A stream;
+
+    if isempty(Nac)
+        Nac = 0;
+        P1A = zeros(2048);
+        stream = 0;
+    end
+
+    if (ready_in)
+        P1A (count) = P1A(count) + P1;
+    end
+    
+    if ready_in & (count == 1)
+        Nac = Nac + 1;
+        if (Nac == 4);
+            stream = 1;
+            Nac = 0;
+        end
+    end
+
+    if stream > 0 
+        ready_out = true;
+        outpk = P1A(stream);
+        P1A(stream) = 0;
+
+        stream = stream + 1;
+        if stream>2048
+            stream  = 0;
+        end
+    else
+        ready_out = false;
+        outpk = 0;
+    end
+
+end
+
