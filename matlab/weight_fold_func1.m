bf154dec714cf4186bb1a84a04e55290ae99d2b2
Anze Slosar <slosar@gmail.com>
2022-12-14 08:28:03 -0500

    somewhat working

--- matlab/weight_fold_func1.m
+++ matlab/weight_fold_func1.m
@@ -1,31 +1,26 @@
 function val = weight_fold_func1 (sample, w1,w2,w3,w4 )
-    persistent buf1 c tout
+    persistent buf1 c tout 
     if isempty(buf1)
-            buf1 = zeros(4, 4096);
+            buf1 = zeros(1, 16384);
             c = uint16(1); % we use 0-ordered for c
-            tout = uint8(4);
+            tout = uint16(1);
     end
 
-    buf1(1,c) = buf1(1,c) + sample * w1;
-    buf1(2,c) = buf1(2,c) + sample * w2;
-    buf1(3,c) = buf1(3,c) + sample * w3;
-    buf1(4,c) = buf1(4,c) + sample * w4;
-            
-    val = buf1(tout,c);
-    buf1 (tout,c) = 0.0;
-            
-    % if (mod(c,4096)==401)
-    %     disp([sample,val,tout])
-    %     disp([w1,w2,w3,w4])
-    % end
+    buf1(c) = buf1(c) + sample * w4;
+    buf1(c+4096) = buf1(c+4096) + sample * w3;
+    buf1(c+8192) = buf1(c+8192) + sample * w2;
+    buf1(c+12288) = buf1(c+12288) + sample * w1;
 
-
+    ndx = (tout-1)*4096+c;
+    val = buf1(ndx);
+    buf1 (ndx) = 0.0;
+            
     c = c+1;
     if c==4097
         c = uint16(1);
-        tout = tout - 1;
-        if tout == 0
-            tout = uint8(4);
+        tout = tout + 1;
+        if tout == 5
+            tout = uint16(1);
         end
     end
 end
