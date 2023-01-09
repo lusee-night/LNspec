071367da2aac58bc389c7e6781eef2ddbcf52f77
Anze Slosar <slosar@gmail.com>
2023-01-09 08:34:46 -0500

    yet another version

--- matlab/src/weight_fold.m
+++ matlab/src/weight_fold.m
@@ -1,38 +1,27 @@
 function val = weight_fold (sample, w1,w2,w3,w4 )
-    persistent buf1 buf2 buf3 buf4 ndx bndx
+    persistent buf ndx bndx
     
-    if isempty(buf1)
-        buf1 = zeros(1, {Nfft});
-        buf2 = zeros(1, {Nfft});
-        buf3 = zeros(1, {Nfft});
-        buf4 = zeros(1, {Nfft});
+    if isempty(buf)
+        buf = zeros(1, {Nblock});
         ndx = 0;
         bndx = 0;
     end
     
     assert({Ntaps}==4);
 
-    if (bndx==0)
-        buf1(ndx+1) = sample;
-    elseif (bndx==1)
-        buf2(ndx+1) = sample;
-    elseif (bndx==2)
-        buf3(ndx+1) = sample;
-    else
-        buf4(ndx+1) = sample;
-    end
+    buf(ndx+1) = sample;
 
-    val = buf1(ndx+1)*w1+buf2(ndx+1)*w2+buf3(ndx+1)*w3+buf4(ndx+1)*w4;
+    val = buf(bndx+1)*w1+buf({Nfft}+bndx+1)*w2+buf({Nfft}*2+bndx+1)*w3+buf({Nfft}*3+bndx+1)*w4;
     
     ndx = ndx + 1;
-    if (ndx=={Nfft})
-        ndx = 0; 
-        bndx = bndx + 1;
-        if (bndx=={Ntaps})
-            bndx=0;
-        end
+    if (ndx=={Nblock})
+        ndx = 0;
     end
-        
+
+    bndx = bndx + 1;
+    if (bndx=={Nfft})
+        bndx=0;
+    end
 end
 
 
