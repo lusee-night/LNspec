3f6b83823b36d3db93790e66e4064b0a8d0225f0
Raguzin <eraguzin@silicon.inst.bnl.gov>
2023-01-19 10:32:18 -0500

    forgot one file

--- matlab/make.m
+++ matlab/make.m
@@ -33,16 +33,18 @@ hdlcfg.EnableRate = "InputDataRate"; %"DUTBaseRate";
 hdlcfg.SimIndexCheck = true;
 hdlcfg.SynthesisToolChipFamily = "polarfire";
 %hdlcfg.SynthesisToolSpeedValue = -1;
-hdlcfg.AdaptivePipelining = true;
-hdlcfg.DistributedPipelining = true;
+%hdlcfg.AdaptivePipelining = true;
+%hdlcfg.DistributedPipelining = true;
 %hdlcfg.InputPipeline = 1;
 %hdlcfg.OutputPipeline = 1;
 
 
 %codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),double(0),double(0),double(0),double(0)} weight_fold__instance_1_
+%codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),double(0),double(0),double(0),double(0)} weight_fold__instance_2_
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0)} sfft 
 %codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),true} deinterlace__instance_12_
-%codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),complex(0,0),int16(0),true,4} average__instance_P1_
+%codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),complex(0,0),int16(0),true} average__instance_P1_
+%codegen -float2fixed fixptcfg -config hdlcfg -args {complex(0,0),complex(0,0),int16(0),true} average__instance_P2_
 codegen -float2fixed fixptcfg -config hdlcfg -args {int16(0),int16(0)} spectrometer
 %codegen -float2fixed fixptcfg -config hdlcfg -args {} weight_streamer
 
