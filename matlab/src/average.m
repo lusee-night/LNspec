0f3487501a005bb74a950f0409bc7141171897f8
Eric Raguzin <eraguzin@bnl.gov>
2023-03-17 17:29:22 -0400

    Changes so that average compiles to VHDL

--- matlab/src/average.m
+++ matlab/src/average.m
@@ -15,7 +15,7 @@ function [outpk, outbin, ready_out] = average(P, bin, ready_in)
     ready_out = false;
     outbin = int16(0);
     outpk = 0;
-    ticktock = mod(bin,2);
+    ticktock = (mod(bin,2)==1);
 
     if ticktock
         assert(mod(bin,2)==1)
@@ -24,21 +24,21 @@ function [outpk, outbin, ready_out] = average(P, bin, ready_in)
         else
             to1adr = int16({Nchan}/2+1);
         end
-        to1val = buf1(to1adr)+P;
+        to1val = buf1(to1adr)+real(P);
         if (Nac == 1) & (ready_in)
             outpk = to1val;
             outbin = bin;
             to1val = 0;
             ready_out=true;
         end
-        buf2(to2adr) = to2val;
+        buf2(to2adr) = real(to2val);
     else
         if (ready_in)
             to2adr = bin/2+1;
         else
             to2adr = int16({Nchan}/2+1);
         end
-        to2val = buf2(to2adr)+P;
+        to2val = buf2(to2adr)+real(P);
         value = to2val;
         if (Nac == 1) & (ready_in)
             outpk = to2val;
@@ -46,7 +46,7 @@ function [outpk, outbin, ready_out] = average(P, bin, ready_in)
             to2val = 0;
             ready_out=true;
         end
-        buf1(to1adr) = to1val;
+        buf1(to1adr) = real(to1val);
     end
 
     Nac = Nac - ((ready_in) & (bin == 0));
