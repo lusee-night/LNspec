719685b08beff3319f19f3423685bf9c69f035b5
Anže Slosar <anze@bnl.gov>
2024-09-20 15:48:26 -0400

    fixes

--- matlab/src/calibrator_tb.m
+++ matlab/src/calibrator_tb.m
@@ -6,8 +6,12 @@ if use_ramp
     Ns=4000
 else
     fprintf("reading data\n")
-    notch_data = read_notch_bin('samples/bethe.input',10000);
-    noise_data = read_notch_bin('samples/bethe.noise',10000);
+    notch_data = read_notch_bin('samples/gamow.input',10000);
+    noise_data2 = read_notch_bin('samples/gamow.noise2',10000);
+    noise_data3 = read_notch_bin('samples/gamow.noise3',10000);
+    noise_data4 = read_notch_bin('samples/gamow.noise4',10000);
+    %var(noise_data2, 0, 1)
+    %var(notch_data, 0, 1)
     Ns = size(notch_data,1);
     fprintf('Ns = %d\n', Ns);
 end
@@ -39,8 +43,8 @@ for ic = 1:Ns
         else
             ch1_notch_real = notch_data(ic,2*jc-1);
             ch1_notch_imag = notch_data(ic,2*jc);
-            ch2_notch_real = noise_data(ic,2*jc-1);
-            ch2_notch_imag = noise_data(ic,2*jc);
+            ch2_notch_real = noise_data2(ic,2*jc-1);
+            ch2_notch_imag = noise_data2(ic,2*jc);
         end
 
         %% readyin means we have data coming from x16
